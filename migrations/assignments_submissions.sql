ba165a31a6ef9bc0d2ff8dd34d0eb1d57758fe02
Duncan Haran <duncan.haran@gmail.com>
2019-07-23 15:11:37 +0000

    new tables and seed data

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -1,11 +1,3 @@
-CREATE TABLE assignment_submissions (
-  id SERIAL PRIMARY KEY NOT NULL,
-  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
-  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
-  duration INTEGER,
-  submission_date DATE
-);
-
 CREATE TABLE assignments (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(200),
@@ -13,4 +5,12 @@ CREATE TABLE assignments (
   day INTEGER,
   chapter INTEGER,
   duration INTEGER
+);
+
+CREATE TABLE assignment_submissions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  duration INTEGER,
+  submission_date DATE
 );
